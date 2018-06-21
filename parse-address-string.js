f49841090c2062123acc21dbcd9b932f71ac5c91
StealthyDev <StealthyDev@gmail.com>
2018-06-21 17:32:29 -0400

    Added ts support

--- parse-address-string.js
+++ parse-address-string.js
@@ -98,7 +98,7 @@ var states
 
 function looksLikeState(str) {
     if (!states) {
-        var map = require('./lib/states.json')
+        var map = input.states;
         states = {}
         for (var k in map) {
             if (map.hasOwnProperty(k)) {
@@ -115,7 +115,7 @@ var countries
 
 function looksLikeCountry(str) {
     if (!countries) {
-        var map = require('./lib/countries.json')
+        var map = input.countries;
         countries = {}
         for (var k in map) {
             if (map.hasOwnProperty(k)) {
@@ -132,3 +132,83 @@ function looksLikeCountry(str) {
 }
 
 export {explodeAddress, implodeAddress}
+
+var input = {
+    states: {
+        "Alabama": "AL"
+        , "Alaska": "AK"
+        , "Arizona": "AZ"
+        , "Arkansas": "AR"
+        , "California": "CA"
+        , "Colorado": "CO"
+        , "Connecticut": "CT"
+        , "Delaware": "DE"
+        , "Florida": "FL"
+        , "Georgia": "GA"
+        , "Hawaii": "HI"
+        , "Idaho": "ID"
+        , "Illinois": "IL"
+        , "Indiana": "IN"
+        , "Iowa": "IA"
+        , "Kansas": "KS"
+        , "Kentucky": "KY"
+        , "Louisiana": "LA"
+        , "Maine": "ME"
+        , "Maryland": "MD"
+        , "Massachusetts": "MA"
+        , "Michigan": "MI"
+        , "Minnesota": "MN"
+        , "Mississippi": "MS"
+        , "Missouri": "MO"
+        , "Montana": "MT"
+        , "Nebraska": "NE"
+        , "Nevada": "NV"
+        , "New Hampshire": "NH"
+        , "New Jersey": "NJ"
+        , "New Mexico": "NM"
+        , "New York": "NY"
+        , "North Carolina": "NC"
+        , "North Dakota": "ND"
+        , "Ohio": "OH"
+        , "Oklahoma": "OK"
+        , "Oregon": "OR"
+        , "Pennsylvania": "PA"
+        , "Rhode Island": "RI"
+        , "South Carolina": "SC"
+        , "South Dakota": "SD"
+        , "Tennessee": "TN"
+        , "Texas": "TX"
+        , "Utah": "UT"
+        , "Vermont": "VT"
+        , "Virginia": "VA"
+        , "Washington": "WA"
+        , "West Virginia": "WV"
+        , "Wisconsin": "WI"
+        , "Wyoming": "WY"
+        , "District of Columbia": "DC"
+
+        , "American Samoa": "AS"
+        , "Guam": "GU"
+        , "Northern Mariana Islands": "MP"
+        , "Puerto Rico": "PR"
+        , "Virgin Islands": "VI"
+
+        , "Alberta": "AB"
+        , "British Columbia": "BC"
+        , "Manitoba": "MB"
+        , "New Brunswick": "NB"
+        , "Newfoundland": "NL"
+        , "Nova Scotia": "NS"
+        , "Northwest Territories": "NT"
+        , "Nunavut": "NU"
+        , "Ontario": "ON"
+        , "Prince Edward Island": "PE"
+        , "Quebec": "QC"
+        , "Saskatchewan": "SK"
+        , "Yukon": "YT"
+    },
+    countries: {
+        "United States": "US"
+        , "Canada": "CA"
+    }
+}
